ac17c89c65185727d138e280cc1ec99442153fc2
Brendan Innis <brendan@wholepunk.com>
2019-06-01 11:19:02 -0700

    using the navigation component

--- app/src/main/java/ca/brendaninnis/murdermysteries/activities/MainActivity.kt
+++ app/src/main/java/ca/brendaninnis/murdermysteries/activities/MainActivity.kt
@@ -16,6 +16,10 @@ import android.transition.Fade
 import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import ca.brendaninnis.murdermysteries.App
 import ca.brendaninnis.murdermysteries.R
 import ca.brendaninnis.murdermysteries.fragments.*
@@ -33,11 +37,15 @@ class MainActivity : AppCompatActivity(),
     private lateinit var mDrawerLayout: DrawerLayout
     private lateinit var mDrawerArrowDrawable: DrawerArrowDrawable
     private lateinit var mNavigationView: NavigationView
+    private lateinit var mNavController: NavController
     private var mBackStackIsEmpty = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mNavigationView = findViewById(R.id.nav_view)
+        mDrawerLayout = findViewById(R.id.drawer_layout)
 
         // New users should get a UUID
         with(getSharedPreferences("meta", Context.MODE_PRIVATE)) {
@@ -53,62 +61,6 @@ class MainActivity : AppCompatActivity(),
             }
         )
 
-        mDrawerLayout = findViewById(R.id.drawer_layout)
-
-        // Set navigation view item selected listener
-        mNavigationView = findViewById(R.id.nav_view)
-        mNavigationView.setNavigationItemSelectedListener { menuItem ->
-            // set item as selected to persist highlight
-            menuItem.isChecked = true
-            // close drawer when item is tapped
-            mDrawerLayout.closeDrawers()
-
-            when(menuItem.itemId) {
-                R.id.nav_play -> {
-                    val fragment = PlayFragment.newInstance().apply {
-                        exitTransition = Fade()
-                        enterTransition = Fade()
-                    }
-                    supportFragmentManager.beginTransaction().run {
-                        replace(R.id.main_content, fragment)
-                        commit()
-                    }
-                }
-                R.id.nav_mysteries -> {
-                    val fragment = MysteriesFragment.newInstance().apply {
-                        exitTransition = Fade()
-                        enterTransition = Fade()
-                    }
-                    supportFragmentManager.beginTransaction().run {
-                        replace(R.id.main_content, fragment)
-                        commit()
-                    }
-                }
-                R.id.nav_how_to_play -> {
-                    supportFragmentManager.beginTransaction().run {
-                        replace(R.id.main_content, HowToPlayFragment.newInstance("param1", "param2"))
-                        commit()
-                    }
-                }
-                R.id.nav_awards -> {
-                    supportFragmentManager.beginTransaction().run {
-                        replace(R.id.main_content, AwardFragment.newInstance("param1", "param2"))
-                        commit()
-                    }
-                }
-                R.id.nav_help -> {
-                    supportFragmentManager.beginTransaction().run {
-                        replace(R.id.main_content, HelpFragment.newInstance())
-                        commit()
-                    }
-                }
-            }
-
-            title = menuItem.title
-
-            true
-        }
-
         // Set toolbar as action bar
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -122,21 +74,67 @@ class MainActivity : AppCompatActivity(),
         supportFragmentManager.apply {
             addOnBackStackChangedListener(this@MainActivity)
         }
+
+        // Set up the nav controller
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, mNavController, mDrawerLayout)
+        NavigationUI.setupWithNavController(mNavigationView, mNavController);
+
+        // Set navigation view item selected listener
+        mNavigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            when(menuItem.itemId) {
+                R.id.nav_play -> {
+                    mNavController.navigate(R.id.playFragment)
+                }
+                R.id.nav_mysteries -> {
+                    mNavController.navigate(R.id.mysteriesFragment)
+                }
+                R.id.nav_how_to_play -> {
+                    mNavController.navigate(R.id.howToPlayFragment)
+                }
+                R.id.nav_awards -> {
+                    mNavController.navigate(R.id.awardFragment)
+                }
+                R.id.nav_help -> {
+                    mNavController.navigate(R.id.helpFragment)
+                }
+            }
+
+            true
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(mNavController, mDrawerLayout)
+    }
+
+    override fun onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun onBackStackChanged() {
-        mBackStackIsEmpty = supportFragmentManager.backStackEntryCount == 0
-
-        // Animate the DrawerArrowDrawable
-        if (mBackStackIsEmpty) {
-            ObjectAnimator.ofFloat(mDrawerArrowDrawable, "progress", 0f).start()
-            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
-            title = getString(R.string.app_name)
-            title = mNavigationView.checkedItem?.title
-        } else {
-            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
-            ObjectAnimator.ofFloat(mDrawerArrowDrawable, "progress", 1f).start()
-        }
+//        mBackStackIsEmpty = supportFragmentManager.backStackEntryCount == 0
+//
+//        // Animate the DrawerArrowDrawable
+//        if (mBackStackIsEmpty) {
+//            ObjectAnimator.ofFloat(mDrawerArrowDrawable, "progress", 0f).start()
+//            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+//            title = getString(R.string.app_name)
+//            title = mNavigationView.checkedItem?.title
+//        } else {
+//            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+//            ObjectAnimator.ofFloat(mDrawerArrowDrawable, "progress", 1f).start()
+//        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -154,38 +152,38 @@ class MainActivity : AppCompatActivity(),
     }
 
     override fun showMystery(mystery: Mystery, view: View) {
-        val fragment = MysteryDetailFragment.newInstance(mystery).apply {
-            sharedElementEnterTransition = DetailsTransition()
-            enterTransition = Fade()
-            sharedElementReturnTransition = DetailsTransition()
-        }
-
-        supportFragmentManager.beginTransaction().run {
-            addSharedElement(view.findViewById<ImageView>(R.id.mystery_splash_image), mystery.name)
-            replace(R.id.main_content, fragment)
-            addToBackStack(null)
-            commit()
-        }
-
-        title = mystery.name
+//        val fragment = MysteryDetailFragment.newInstance(mystery).apply {
+//            sharedElementEnterTransition = DetailsTransition()
+//            enterTransition = Fade()
+//            sharedElementReturnTransition = DetailsTransition()
+//        }
+//
+//        supportFragmentManager.beginTransaction().run {
+//            addSharedElement(view.findViewById<ImageView>(R.id.mystery_splash_image), mystery.name)
+//            replace(R.id.main_content, fragment)
+//            addToBackStack(null)
+//            commit()
+//        }
+//
+//        title = mystery.name
     }
 
     override fun createParty(mystery: Mystery, splashImage: View, button: View) {
-        val fragment = NewPartyFragment.newInstance(mystery).apply {
-            sharedElementEnterTransition = DetailsTransition()
-            enterTransition = Fade()
-            sharedElementReturnTransition = DetailsTransition()
-        }
-
-        supportFragmentManager.beginTransaction().run {
-//            addSharedElement(splashImage, mystery.name)
-            addSharedElement(button, "host_party_button")
-            replace(R.id.main_content, fragment)
-            addToBackStack(null)
-            commit()
-        }
-
-        title = getString(R.string.new_party)
+//        val fragment = NewPartyFragment.newInstance(mystery).apply {
+//            sharedElementEnterTransition = DetailsTransition()
+//            enterTransition = Fade()
+//            sharedElementReturnTransition = DetailsTransition()
+//        }
+//
+//        supportFragmentManager.beginTransaction().run {
+////            addSharedElement(splashImage, mystery.name)
+//            addSharedElement(button, "host_party_button")
+//            replace(R.id.main_content, fragment)
+//            addToBackStack(null)
+//            commit()
+//        }
+//
+//        title = getString(R.string.new_party)
     }
 
     override fun partySelected() {
