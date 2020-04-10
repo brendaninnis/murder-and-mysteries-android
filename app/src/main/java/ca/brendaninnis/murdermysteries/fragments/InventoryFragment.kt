a26cf152d70784f4b64326be7be2ce2bdff88a62
Brendan Innis <innisbrendan@gmail.com>
2020-04-10 15:59:57 -0700

    added fragments to nav graph for before murder menu

---
+++ app/src/main/java/ca/brendaninnis/murdermysteries/fragments/InventoryFragment.kt
@@ -0,0 +1,23 @@
+package ca.brendaninnis.murdermysteries.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ca.brendaninnis.murdermysteries.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class InventoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inventory, container, false)
+    }
+
+}
